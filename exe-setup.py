6c88554453cf084a53639e12dc7064581ebea2ec
Chad <chad-dev@katica.org>
2011-09-22 03:21:02 +0000

    exe-setup.py    set up for gui app with icon
    samples/simp-wikipedia-Confucius.txt
    samples/trad-wikipedia-Confucius.txt
      - Added sample articles from wikipedia
    
    wxapp/ui/prefsdialog.py
      - removed debugging prints

--- exe-setup.py
+++ exe-setup.py
@@ -1,4 +1,14 @@
 from distutils.core import setup
 import py2exe
 
-setup(console=['VocabExtractorDev.py'])
+# setup(console=['main.py'])
+
+
+setup(
+    windows = [
+        {
+            "script": "main.py",
+            "icon_resources": [(1, "application-icon.ico")]
+        }
+    ],
+)
