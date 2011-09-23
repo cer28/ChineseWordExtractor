b65c152f64c6184727c511811dc98e4b7012c250
Chad <Chad@988707d8-0148-4298-b990-f3c2f4f2a650>
2011-09-23 04:09:24 +0000

    New icon set; set icon for main frame

--- trunk/wxapp/__init__.py
+++ trunk/wxapp/__init__.py
@@ -19,7 +19,18 @@ def run(segHelper):
                       default=os.path.expanduser("~/Chinese Word Extractor"))
     (opts, args) = parser.parse_args(sys.argv[1:])
 
+
     app = wx.PySimpleApp()
+
+    # Notes on icon bundles:
+    # 1) can't be 256x256 because too big to import within the application
+    # 2) the 16x16 object will be the shown in taskbar , Explorer list view, application context menu, etc.
+    #     - It will choose the 16-color icon if it exists. I don't know why anyone would want this
+    #     - the 256 color works fine. I haven't tried other sizes.
+
+    # pre-load icons; they need to be set for the load dictionary progress, since it happens before the main frame shows
+    ib = wx.IconBundle()
+    ib.AddIconFromFile("application-icon.ico", wx.BITMAP_TYPE_ANY)
 
     # configuration
     config = config.Config(
@@ -27,6 +38,7 @@ def run(segHelper):
     config.appDir = segHelper.runningDir
 
     prog = wx.ProgressDialog(parent=None, title="Progress", message="Loading Dictionary", style=wx.PD_AUTO_HIDE|wx.PD_SMOOTH)
+    prog.SetIcons(ib)
     segHelper.config = config
     segHelper.LoadData(updatefunction=prog.Update)
     prog.Destroy()
@@ -37,6 +49,9 @@ def run(segHelper):
     #loads main window
     ui.importAll()
     frame = ui.main.MainWindow(None, -1, "Chinese Word Extractor", size=(750,500))
+
+    frame.SetIcons(ib)
+    
     frame.segHelper = segHelper
     frame.config = config
     frame.notebook.messagePanel.SetValue(frame.segHelper.getMessages())
