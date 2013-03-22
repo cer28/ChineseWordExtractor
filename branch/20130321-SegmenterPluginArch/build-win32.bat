cb416399fb45a0e34f77330be6bf1d5091ebf6d3
Chad <Chad@988707d8-0148-4298-b990-f3c2f4f2a650>
2013-03-22 00:36:04 +0000

    New experimental branch for segmenter plugin architecture

---
+++ branch/20130321-SegmenterPluginArch/build-win32.bat
@@ -0,0 +1,33 @@
+set PATH=C:\Program Files\Python27;c:\Program Files\TortoiseSVN\bin;%PATH%
+
+RMDIR /S /Q stage
+
+@REM BASE URL is http://svn.zhtoolkit.com/ChineseWordExtractor/tags/release-0_3_0
+
+REM **Make sure to choose the proper tags/release-* directory as the source
+TortoiseProc.exe /command:export /path:stage
+
+@REM TortoiseProc.exe /command:dropexport /path:http://svn.zhtoolkit.com/ChineseWordExtractor/tags/release-0_3_0 /droptarget:stage
+
+pause
+
+cd stage
+
+
+copy ..\Microsoft.VC90.CRT.manifest .\
+copy ..\msvcm90.dll .\
+copy ..\msvcp90.dll .\
+copy ..\msvcr90.dll .\
+
+
+python exe-setup.py py2exe
+
+
+chdir dist
+del "Chinese Word Extractor.exe"
+ren main.exe "Chinese Word Extractor.exe"
+
+cd ..
+ren dist "Chinese Word Extractor"
+
+REM Now zip the folder "Chinese Word Extractor", and rename the zip file
